06dab079040654eff388a5a3b28e5c9b98ba4094
nezarabouhamdan <nwa200079@gmail.com>
2023-05-04 15:13:40 +0300

    last

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,145 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Background from "../assets/MetaV _ Website.png";
+import Image1 from "../assets/Group 48095504.png";
+import {
+  Button,
+  Desc,
+  Head,
+  Head2,
+  Heroimg,
+  Herosection,
+  Image,
+  Imagediv,
+  Item,
+  Links,
+  Navbar,
+  Row,
+  Textdiv,
+} from "./Herostyles";
+import VisibilitySensor from "react-visibility-sensor";
+import NumberCounter from "number-counter";
+import "../App.css";
+import { useAnimation, useInView } from "framer-motion";
 function Hero() {
-  return <div>Hero</div>;
+  const initial = { opacity: 0, y: -20 };
+  const animation = useAnimation();
+  const { ref, inView } = useInView({ threshold: 0.2 });
+  useEffect(() => {
+    if (!inView) {
+      console.log("sss");
+      animation.start({
+        opacity: 1,
+        y: 1,
+      });
+    }
+  }, [inView, animation]);
+
+  return (
+    <>
+      <Herosection ref={ref}>
+        <Heroimg src={Background} />
+        <Navbar
+          initial={initial}
+          transition={{ delay: 0.3, duration: 0.5 }}
+          animate={animation}
+        >
+          <Row margin="true"> MetaV</Row>
+          <Row>
+            <Links color="Gradient">Home</Links>
+            <Links>Resourse</Links>
+            <Links>Pricing</Links>
+            <Links>About us</Links>
+          </Row>
+        </Navbar>
+        <Row>
+          <Textdiv>
+            <Head
+              initial={initial}
+              transition={{ delay: 0.6, duration: 0.5 }}
+              animate={animation}
+            >
+              Explore the future of<br></br>
+              <Row>
+                the<Head color="red">Meatverse</Head>{" "}
+              </Row>
+            </Head>
+            <Head2
+              initial={initial}
+              transition={{ delay: 0.9, duration: 0.5 }}
+              animate={animation}
+            >
+              Let's explore and create your experience with metaverse.
+            </Head2>
+            <Row
+              initial={initial}
+              transition={{ delay: 1.2, duration: 0.5 }}
+              animate={animation}
+            >
+              <Button color="s">GET STARTED</Button>
+              <Button>EXPLORE NOW</Button>
+            </Row>
+            <Row>
+              <VisibilitySensor partialVisibility offset={{ bottom: 10 }}>
+                {({ isVisible }) => (
+                  <Item>
+                    {isVisible ? (
+                      <NumberCounter
+                        end={29}
+                        delay={2.5}
+                        className="increment"
+                        // preFix="Up revenue:"
+                        postFix="K+"
+                      />
+                    ) : null}{" "}
+                    <Desc> Artworks</Desc>
+                  </Item>
+                )}
+              </VisibilitySensor>{" "}
+              <VisibilitySensor partialVisibility offset={{ bottom: 10 }}>
+                {({ isVisible }) => (
+                  <Item>
+                    {isVisible ? (
+                      <NumberCounter
+                        end={18}
+                        delay={2.5}
+                        className="increment"
+                        // preFix="Up revenue:"
+                        postFix="K+"
+                      />
+                    ) : null}{" "}
+                    <Desc> Artists</Desc>
+                  </Item>
+                )}
+              </VisibilitySensor>{" "}
+              <VisibilitySensor partialVisibility offset={{ bottom: 10 }}>
+                {({ isVisible }) => (
+                  <Item>
+                    {isVisible ? (
+                      <NumberCounter
+                        end={25}
+                        delay={2.5}
+                        className="increment"
+                        // preFix="Up revenue:"
+                        postFix="K+"
+                      />
+                    ) : null}{" "}
+                    <Desc> Autcion</Desc>
+                  </Item>
+                )}
+              </VisibilitySensor>
+            </Row>
+          </Textdiv>
+          <Imagediv
+            initial={initial}
+            transition={{ delay: 1.2, duration: 0.5 }}
+            animate={animation}
+          >
+            <Image src={Image1}></Image>
+          </Imagediv>
+        </Row>
+      </Herosection>
+    </>
+  );
 }
 
 export default Hero;
---
+++ src/Components/Herostyles.js
@@ -0,0 +1,145 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+export const Herosection = styled.div`
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  object-fit: cover;
+  color: white;
+  flex-direction: column;
+  justify-content: left;
+  align-content: left;
+  align-items: left;
+  @media screen and (max-width: 968px) {
+    height: 100vh;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+  }
+`;
+
+export const Heroimg = styled.img`
+  object-fit: fill;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  z-index: -1;
+`;
+export const Navbar = styled(motion.div)`
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  flex-direction: row;
+  font-family: "Orbitron SemiBold";
+
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Row = styled(motion.div)`
+  margin-left: ${({ margin }) => (margin ? "20px" : "0")};
+
+  display: flex;
+  flex-direction: row;
+`;
+export const Links = styled(motion.h3)`
+  margin: auto 20px;
+  font-size: 18px;
+  cursor: pointer;
+  font-family: "Orbitron SemiBold";
+
+  background: ${({ color }) =>
+    color ? "linear-gradient(to right,#F86CF8, #64CEF5);" : "white"};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+export const Textdiv = styled.div`
+height:60%;
+width 47%;
+diplay:flex;
+margin:auto;
+flex-direction:column;
+`;
+export const Head = styled(motion.div)`
+  font-size: 50px;
+  background: ${({ color }) =>
+    color ? "linear-gradient(to right,#F86CF8, #64CEF5);" : "white"};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-family: "Orbitron SemiBold";
+  font-weight: 700;
+`;
+export const Head2 = styled(motion.div)`
+  margin-top: 5px;
+  font-size: 15px;
+`;
+export const Button = styled(motion.div)`
+  height: 50px;
+  width: 160px;
+  display: flex;
+  flex-direcation: row;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: 25px;
+  margin-left: 10px;
+  border-radius: 8px;
+  font-family: "Orbitron SemiBold";
+
+  background: ${({ color }) =>
+    color ? "linear-gradient(to right,#D10ED1, #10A3DA);" : "none"};
+
+  border: 2px solid ${({ color }) => (color ? "none" : "  #10a3da;")};
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  &:before {
+    background: ${({ color }) => (color ? "#fff" : "#fff")};
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: -1;
+    transition: all 0.6s ease;
+    width: 102%;
+    height: 0%;
+  }
+  &:hover:before {
+    height: 200%;
+  }
+  &:hover {
+    z-index: 1;
+
+    color: ${({ color }) => (color ? "black" : "black")};
+  }
+`;
+export const Imagediv = styled(motion.div)`
+height:100%;
+width 40%;`;
+export const Image = styled.img`
+margin-top:15%;
+height:80%;
+width 70%;`;
+
+export const Item = styled.div`
+  width: 10vw;
+  z-index: 1;
+  height: 32vh;
+  display: flex;
+  flex-direction: Column;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 968px) {
+    height: 30vh;
+  }
+`;
+
+export const Desc = styled.h2`
+  font-size: 15px;
+  color: white;
+  font-family: "Orbitron SemiBold";
+
+  @media screen and (max-width: 968px) {
+    font-size: 15px;
+  }
+`;
